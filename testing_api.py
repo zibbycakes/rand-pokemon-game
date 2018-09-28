12442e90e536014bc5ed9960fdf121fdc85c3825
Allyson Walpole <zibbycakes@gmail.com>
2018-09-28 16:00:08 -0400

    messing around with the pokeapi.co api with python bc it's simpler

---
+++ testing_api.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+response = requests.get("https://pokeapi.co/api/v2/generation/1/")
+print(response.status_code)
+
+response_json = response.json()
+print(response_json['pokemon_species'])
+
+pokemon_db_a =  {} # { id: name }
+
+for species in response_json['pokemon_species'] :
+    poke_url = species['url']
+    pokemon_id = int(poke_url[poke_url.find('-species/')+9:len(poke_url)-1])
+    # print(pokemon_id)
+    pokemon_db_a[species['name']] = {'id': pokemon_id, 'gen': '1'}
+
+print(pokemon_db_a)
+print(pokemon_db_a['mankey'])
